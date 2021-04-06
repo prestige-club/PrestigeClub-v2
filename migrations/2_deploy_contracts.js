59ec42e79bbb38acc8990d631aa40b930bf0287f
prestige-club <prestigeadmin21@protonmail.com>
2021-04-06 19:49:10 +0200

    Added Migrations for L2 and new DEX

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,13 +1,31 @@
 const SafeMath = artifacts.require("SafeMath");
-const Context = artifacts.require("Context");
 const PEthDex = artifacts.require("PEthDex");
+const WETH = artifacts.require("WETH");
 
-module.exports = function(deployer) {
+module.exports = async function(deployer, network) {
   // deployer.deploy(ConvertLib);
   // deployer.link(ConvertLib, MetaCoin);
   // deployer.deploy(MetaCoin);
 
-  deployer.deploy(SafeMath)
-  deployer.link(SafeMath, PEthDex)
-  deployer.deploy(PEthDex)
+  deployer.deploy(SafeMath);
+  deployer.link(SafeMath, PEthDex);
+  
+  let weth_addr = ""
+
+  if(network == "mumbai"){
+
+    weth_addr = "0xA6FA4fB5f76172d178d61B04b0ecd319C5d1C0aa";
+
+  }else if(["develop", "development", "local", "ganache"].includes(network)){
+
+    await deployer.deploy(WETH);
+    let we = await WETH.deployed(); 
+    console.log("WETH: " + we.address)
+    weth_addr = we.address;
+
+  }else{
+    throw "Network is not support from Migration 2";
+  }
+
+  await deployer.deploy(PEthDex, weth_addr);
 };
--- migrations/5_deploy_seller_real.js
+++ migrations/5_deploy_seller_real.js
@@ -5,6 +5,7 @@ const Seller = artifacts.require("AccountExchange");
 module.exports = async function(deployer, network, accounts) {
 
     let pc = await PrestigeClub.deployed()
+    // await deployer.deploy(Seller, "0x4406deba31861a67d291189ac74f734bb0b1cef2"); //{from: accounts[1]}
     await deployer.deploy(Seller, pc.address); //{from: accounts[1]}
 
     let seller = await Seller.deployed()
