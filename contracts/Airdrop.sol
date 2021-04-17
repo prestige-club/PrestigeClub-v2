0958bc3f1170062edf20edc4cdd9f49972be47af
prestige-club <prestigeadmin21@protonmail.com>
2021-04-17 17:42:37 +0200

    Created Airdrop Contract

---
+++ contracts/Airdrop.sol
@@ -0,0 +1,34 @@
+
+pragma solidity 0.6.8;
+
+// SPDX-License-Identifier: MIT
+
+import "./Ownable.sol";
+import "./IERC20.sol";
+
+contract Airdrop is Ownable {
+
+  function drop(IERC20 token, address[] memory recipients, uint256[] memory values) public onlyOwner {
+    for (uint256 i = 0; i < recipients.length; i++) {
+      token.transfer(recipients[i], values[i]);
+    }
+  }
+  
+  function dropETH(address[] memory recipients, uint256[] memory values) public onlyOwner {
+    for (uint256 i = 0; i < recipients.length; i++) {
+      payable(recipients[i]).transfer(values[i]);
+    }
+  }
+
+  function rescueErc20(address erc20, uint256 amount) external onlyOwner {
+      IERC20(erc20).transfer(owner(), amount);
+  }
+
+  function rescueEth() external onlyOwner {
+      payable(owner()).transfer(address(this).balance);
+  }
+
+  function kill() external onlyOwner {
+    selfdestruct(payable(owner()));
+  }
+}
